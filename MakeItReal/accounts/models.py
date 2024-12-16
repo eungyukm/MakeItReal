81dcc2234cd33271f5fa17e9ab79185a43d904a8
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2024-12-16 22:18:50 +0900

    로그인 모델 생성 및 로그인 페이지 생성

--- MakeItReal/accounts/models.py
+++ MakeItReal/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(default=now)
+    is_active = models.BooleanField()
